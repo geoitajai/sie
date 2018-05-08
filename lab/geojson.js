b3a2778e34b430894f0cb319cd2f2cb8595cefa0
jaceguay <jaceguay@gmail.com>
2018-05-08 17:20:13 -0300

    mapa FAMAI

--- lab/geojson.js
+++ lab/geojson.js
@@ -1,25 +1,33 @@
-var mapdot = [
-    {
-        "type": "Feature",
-        "properties": {
-            "name": "teste1"
+let mapdot = {
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "teste1"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [7021246,731065]
+            }
         },
-        "geometry": {
-            "type":"Point",
-            "coordinates": [-48.67327210,-26.91146930]
-        }
-    },
-    {
-        "type": "Feature",
-        "properties": {
-            "name": "teste2"
-        },
-        "geometry": {
-            "type":"Point",
-            "coordinates": [-48.67511430,-26.92226570]
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "teste2"
+            },
+            "geometry": {
+                "type":"Point",
+                "coordinates": [7020053,-26.92226570]
+            }
+        }],
+    "type": "FeatureCollection",
+    "crs": {
+        "type": "name",
+        "properties":{
+            "name":"urn:ogc:def:crs:EPSG::31982"
         }
     }
-];
+};
 //console.log(mapdot[0].type);
-mapdot[0].crs = { 'type': 'name', 'properties': { 'name': 'urn:ogc:def:crs:EPSG::31982' } }
+//mapdot[0].crs = { 'type': 'name', 'properties': { 'name': 'urn:ogc:def:crs:EPSG::31982' } }
 console.log(mapdot)
