96878d9fddeff53abf7ca3fc099f5bcad8e31ac8
jaceguay <jaceguay@gmail.com>
2018-10-23 23:45:36 -0300

    canvas test

--- lab/labestudo/data/divisaolotes/divlotes.js
+++ lab/labestudo/data/divisaolotes/divlotes.js
@@ -3,13 +3,13 @@ const plantaLotes = JSON.parse(fs.readFileSync('./plantacadastral.geojson', 'utf
 
 //obter a lista única, nome das articulações
 const listaArticula = Array.from(new Set(plantaLotes.features.map((x) => {
-    return x.properties.plantacadastralpontos_nome //nome da articulação
+    return x.properties.retic //nome da articulação
 })));
 
 //itens agrupados
 let resultado = plantaLotes.features.reduce((grupo, item) => {
-    grupo[item.properties.plantacadastralpontos_nome] = grupo[item.properties.plantacadastralpontos_nome] || []//se não existir o nome do elemento, criar um vazio, se existir push
-    grupo[item.properties.plantacadastralpontos_nome].push(
+    grupo[item.properties.retic] = grupo[item.properties.retic] || []//se não existir o nome do elemento, criar um vazio, se existir push
+    grupo[item.properties.retic].push(
         item
     )
     return grupo
